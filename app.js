b880edbe1cc88e1b416bfb2d8cffa2af49c0819e
Sergio Gordillo <54623826+SergioGordillo@users.noreply.github.com>
2020-08-02 22:48:27 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+let nombre = "Sergio";
+
+//console.log("Hola " + nombre); //Esto sería en JS 
+
+console.log(`Hola ${nombre}`); //De esta forma hago una inyección en el String, y así se hace con Node.js
